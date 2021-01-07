8bcc41c84941f827e08572c6bd579fd757864844
DvCMarques <dvcmarques@gmail.com>
2021-01-07 11:02:21 +0000

    Added SceneLoadButton
    Added SceneLoadButton and editor scripts
    Added TMPro for proper UI

---
+++ Sam the Knight/Assets/_Scripts/Editor/SceneLoadButtonEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(SceneLoadButton))]
+public class SceneLoadButtonEditor :  ButtonEditor{
+
+    public override void OnInspectorGUI() {
+       
+        SceneLoadButton targetButton = (SceneLoadButton)target;
+
+        GUILayout.Label("Select a Scene to Load when clicked");
+        targetButton.TargetSceneName = EditorGUILayout.TextField("Target Scene", targetButton.TargetSceneName);
+
+
+        EditorGUILayout.Space();
+        base.OnInspectorGUI();
+    }
+
+}
---
+++ Sam the Knight/Assets/_Scripts/SceneLoadButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SceneLoadButton : Button
+{
+    public string TargetSceneName {
+        get =>  _targetSceneName;
+        set => _targetSceneName = value;
+     }
+    [SerializeField]
+    string _targetSceneName;
+
+    protected override void Awake() {
+        base.Awake();
+        this.onClick.AddListener(() => {
+            SceneManager.LoadSceneAsync(_targetSceneName);
+        });
+    }
+
+}
